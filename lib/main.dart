c420905664d5d166fbe9b3fea143f6e09d591a62
Mark Frankli <mark@pearscom.com>
2025-02-09 10:43:27 +0100

    merge

--- lib/main.dart
+++ lib/main.dart
@@ -99,11 +99,7 @@ class WelcomePage extends StatelessWidget {
       loggedIn = false;
     }
 
-<<<<<<< HEAD
-    final ({List<String> activities, List<Map<String, dynamic>> gyms}) actAndGyms = await helpers.getActivitiesAndGyms();
-=======
     final ActGymRecord actAndGyms = await helpers.getActivitiesAndGyms();
->>>>>>> mark-dev
     return (activities: actAndGyms.activities, gyms: actAndGyms.gyms, loggedIn :loggedIn);
   }
 
