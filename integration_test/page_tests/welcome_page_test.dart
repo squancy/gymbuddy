3f177b2fd3a3dba85983a2bc57b415ef94ed299d
Mark Frankli <mark@pearscom.com>
2025-02-09 23:01:20 +0100

    reformatted and renamed welcome_page_test.dart

--- integration_test/page_tests/welcome_page_test.dart
+++ integration_test/page_tests/welcome_page_test.dart
@@ -8,40 +8,35 @@ import 'package:gym_buddy/utils/helpers.dart' as helpers;
 Future<void> main() async {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
   await helpers.firebaseInit(test: true);
-  group("Welcomepage testing", () {
-    testWidgets('Welcomepage test for login navigation',
-        (WidgetTester tester) async {
-      await tester.pumpWidget(
-          MaterialApp(home: WelcomePage(key: const Key("welcomePage1"))));
-      print("Welcome page loaded");
-      await tester.pump(Duration(seconds: 5));
+  group("Welcome page testing", () {
+    testWidgets('Welcomepage test for login navigation', (WidgetTester tester) async {
+      await tester.pumpWidget(MaterialApp(home: WelcomePage(key: const Key("welcomePage1"))));
 
+      /*
+        pumpAndSettle() cannot be used here since there is a background animation running forever
+      */
+      await tester.pump(Duration(seconds: 5));
       final loginBtn = find.text(HomeConsts.loginButtonTitle);
-      print("Login button found");
       expect(loginBtn, findsOneWidget);
+
       await tester.tap(loginBtn);
       await tester.pumpAndSettle();
-      print("Navigated to login page");
       expect(find.text(LoginConsts.mainScreenText), findsOneWidget);
     });
 
-    //Navigating back to Welcome page
-    testWidgets("Welcomepage test for signup navigation",
-        (WidgetTester tester) async {
+    // Navigating back to Welcome page
+    testWidgets("Welcomepage test for signup navigation", (WidgetTester tester) async {
+      // Reload the page and reset states
       await tester.pumpWidget(Container());
       await tester.pumpAndSettle();
-
-      await tester.pumpWidget(
-          MaterialApp(home: WelcomePage(key: const Key("welcomePage5"))));
-      print("Welcome page loaded again");
+      await tester.pumpWidget(MaterialApp(home: WelcomePage(key: const Key("welcomePage2"))));
       await tester.pump(Duration(seconds: 5));
 
       final signupBtn = find.text(HomeConsts.signupButtonTitle);
-      print("Signup button found");
       expect(signupBtn, findsOneWidget);
+
       await tester.tap(signupBtn);
       await tester.pumpAndSettle();
-      print("Navigated to signup page");
       expect(find.text(SignupConsts.mainScreenText), findsOneWidget);
     });
   });
--- lib/consts/common_consts.dart
+++ lib/consts/common_consts.dart
@@ -28,7 +28,7 @@ class SignupConsts {
 
 // Home page [main.dart]
 class HomeConsts {
-  static const String appTitle = 'Gym Buddy App'; 
+  static const String appTitle = 'Kagur'; 
   static const String loginButtonTitle = 'Log in';
   static const String signupButtonTitle = 'Sign up';
 }
