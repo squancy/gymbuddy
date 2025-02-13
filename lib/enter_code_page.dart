ba23a96a533e8e34f64f4568c3931aedcf360279
erikhalasz <erik.halasz02@gmail.com>
2025-02-13 14:17:52 +0100

    TODO ID '110' Added timer button to resend code if necessary. Right now it is 60 seconds. Have not tested yet.

--- lib/enter_code_page.dart
+++ lib/enter_code_page.dart
@@ -4,6 +4,10 @@ import 'package:gym_buddy/renew_password.dart';
 import 'consts/common_consts.dart';
 import 'package:moye/widgets/gradient_overlay.dart';
 import 'utils/helpers.dart' as helpers;
+import 'package:gym_buddy/utils/test_utils/test_helpers.dart' as test_helpers;
+import 'package:gym_buddy/utils/email.dart' as email_send;
+import 'package:gym_buddy/consts/email_templates.dart';
+import 'package:timer_button/timer_button.dart';
 
 class EnterCodePage extends StatefulWidget {
   final String email;
@@ -19,19 +23,21 @@ class _EnterCodePageState extends State<EnterCodePage> {
   final FocusNode _codeFocusNode = FocusNode();
   final ValueNotifier<String> _codeStatus = ValueNotifier<String>("");
   final FirebaseFirestore db = FirebaseFirestore.instance;
+  final ValueNotifier<String> _forgotPassStatus = ValueNotifier<String>("");
 
   /// Make sure the user enters the correct code (=temporary password) received in email
   Future<void> _checkCode() async {
     final String code = _codeController.text;
-    final String email = widget.email; 
+    final String email = widget.email;
 
     // Get the ID of the potential user
-    final userDocs = (await db.collection('users')
-      .where('email', isEqualTo: email)
-      .where('temp_pass', isEqualTo: code)
-      .get())
-      .docs;
-    
+    final userDocs = (await db
+            .collection('users')
+            .where('email', isEqualTo: email)
+            .where('temp_pass', isEqualTo: code)
+            .get())
+        .docs;
+
     if (userDocs.isEmpty) {
       setState(() {
         _codeStatus.value = ForgotPasswordConsts.codePageErrorText;
@@ -46,8 +52,9 @@ class _EnterCodePageState extends State<EnterCodePage> {
         Navigator.push(
           context,
           MaterialPageRoute(
-            builder: (context) => RenewPasswordPage(userID: userID,)
-          ),
+              builder: (context) => RenewPasswordPage(
+                    userID: userID,
+                  )),
         );
       });
     } else {
@@ -55,6 +62,40 @@ class _EnterCodePageState extends State<EnterCodePage> {
         _codeStatus.value = GlobalConsts.unknownErrorText;
       });
     }
+  }
+
+  Future<void> _sendPassword() async {
+    final String email = widget.email;
+    final FirebaseFirestore db = FirebaseFirestore.instance;
+
+    // Make sure the email is in db
+    final userData =
+        (await db.collection('users').where('email', isEqualTo: email).get()).docs.toList();
+
+    // Generate a temporary password: user can change it later in their profile
+    final tempPass = test_helpers.generateRandomString(10);
+
+    final username = userData[0].data()['username'];
+    final userID = userData[0].reference.id;
+    final TemporaryPassEmail tempPassEmail =
+        TemporaryPassEmail(username: username, tempPass: tempPass);
+
+    // Send temporary password to user's email address
+    try {
+      await email_send.sendEmail(
+          from: GlobalConsts.infoEmail,
+          to: email,
+          subject: tempPassEmail.subject,
+          content: tempPassEmail.generateEmail());
+    } catch (e) {
+      setState(() {
+        _forgotPassStatus.value = GlobalConsts.unknownErrorText;
+      });
+      return;
+    }
+
+    // Set temporary password in db
+    await db.collection('users').doc(userID).update({'temp_pass': tempPass});
   }
 
   @override
@@ -71,75 +112,74 @@ class _EnterCodePageState extends State<EnterCodePage> {
       body: Padding(
         padding: const EdgeInsets.all(20.0),
         child: LayoutBuilder(
-          builder: (context, constraints) => SingleChildScrollView(
-            child: Container(
-              constraints: BoxConstraints(
-                minHeight: constraints.maxHeight
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 0),
-                    child: Text(
-                      ForgotPasswordConsts.codePageMainText, 
-                      style: TextStyle(
-                        fontSize: 34,
-                        color: Theme.of(context).colorScheme.primary,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ).withGradientOverlay(gradient: LinearGradient(colors: [
-                      Theme.of(context).colorScheme.primary,
-                      Theme.of(context).colorScheme.tertiary,
-                      Theme.of(context).colorScheme.primary,
-                    ])),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(0, 0, 0, 10),
-                    child: Text(
-                      ForgotPasswordConsts.codePageInfoText,
-                      style: TextStyle(color: Theme.of(context).colorScheme.onSurface)
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 0),
-                    child: helpers.BlackTextfield(
-                      context,
-                      'Code',
-                      _codeController,
-                      _codeFocusNode,
-                      isPassword: false,
-                      isEmail: true
-                    )
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 0),
-                    child: SizedBox(
-                      height: 45,
-                      child: helpers.ProgressBtn(
-                        onPressedFn: _checkCode,
-                        child: Text('Confirm')
-                      )
-                    ),
-                  ),
-                  ValueListenableBuilder<String>(
-                    valueListenable: _codeStatus,
-                    builder: (BuildContext context, String value, Widget? child) {
-                      return Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 0),
-                        child: Text(
-                          value,
-                          textAlign: TextAlign.center,
-                          style: TextStyle(color: Theme.of(context).colorScheme.onSurface),
+            builder: (context, constraints) => SingleChildScrollView(
+                  child: Container(
+                    constraints: BoxConstraints(minHeight: constraints.maxHeight),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 0),
+                          child: Text(
+                            ForgotPasswordConsts.codePageMainText,
+                            style: TextStyle(
+                              fontSize: 34,
+                              color: Theme.of(context).colorScheme.primary,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ).withGradientOverlay(
+                              gradient: LinearGradient(colors: [
+                            Theme.of(context).colorScheme.primary,
+                            Theme.of(context).colorScheme.tertiary,
+                            Theme.of(context).colorScheme.primary,
+                          ])),
                         ),
-                      );
-                    },
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(0, 0, 0, 10),
+                          child: Text(ForgotPasswordConsts.codePageInfoText,
+                              style: TextStyle(color: Theme.of(context).colorScheme.onSurface)),
+                        ),
+                        Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 0),
+                            child: helpers.BlackTextfield(
+                                context, 'Code', _codeController, _codeFocusNode,
+                                isPassword: false, isEmail: true)),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 0),
+                          child: SizedBox(
+                              height: 45,
+                              child: helpers.ProgressBtn(
+                                  onPressedFn: _checkCode, child: Text('Confirm'))),
+                        ),
+                        ValueListenableBuilder<String>(
+                          valueListenable: _codeStatus,
+                          builder: (BuildContext context, String value, Widget? child) {
+                            return Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 0),
+                              child: Text(
+                                value,
+                                textAlign: TextAlign.center,
+                                style: TextStyle(color: Theme.of(context).colorScheme.onSurface),
+                              ),
+                            );
+                          },
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 0),
+                          child: TimerButton(
+                            label: "Resend code",
+                            timeOutInSeconds: 60,
+                            onPressed: _sendPassword,
+                            disabledColor: Theme.of(context).colorScheme.surface,
+                            color: Theme.of(context).colorScheme.secondary,
+                            disabledTextStyle: TextStyle(color: const Color.fromARGB(255, 145, 144, 144)),
+                            activeTextStyle: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                ],
-              ),
-            ),
-          )
-        ),
+                )),
       ),
     );
   }
